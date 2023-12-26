95fcd0c04b64a2072d486b82c1e0593d8f47efc0
001Rakib <rakibrakibrakib354@gmail.com>
2023-12-26 15:54:28 +0600

    added testimonial slider to the homepage

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -23,6 +23,11 @@ const PopularMenu = () => {
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
       </div>
+      <div className="text-center">
+        <button className="btn btn-outline border-0 border-b-4 mt-8">
+          View Full Menu
+        </button>
+      </div>
     </section>
   );
 };
