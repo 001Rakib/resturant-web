4492ed9e093aa7344cadef64479364f6488e42cb
001Rakib <rakibrakibrakib354@gmail.com>
2023-12-31 18:58:41 +0600

    implemented login and sign up functionality

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -1,6 +1,16 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const NavBar = () => {
+  const { user, logOut } = useContext(AuthContext);
+
+  const handleLogOut = () => {
+    logOut()
+      .then(() => {})
+      .catch((error) => console.log(error));
+  };
+
   const navOptions = (
     <>
       <li>
@@ -13,8 +23,23 @@ const NavBar = () => {
         <Link to={"/order/salad"}>Order Food</Link>
       </li>
       <li>
-        <Link to={"/login"}>Login</Link>
+        <Link to={"/signUp"}>Sign Up</Link>
       </li>
+
+      {user ? (
+        <>
+          {" "}
+          <button onClick={handleLogOut} className="btn btn-ghost">
+            Logout
+          </button>{" "}
+        </>
+      ) : (
+        <>
+          <li>
+            <Link to={"/login"}>Login</Link>
+          </li>
+        </>
+      )}
     </>
   );
   return (
